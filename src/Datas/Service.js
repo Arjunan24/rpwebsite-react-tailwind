2b34b8c4f8782461f89e3bc8174a018ac5121f95
Arjunan95 <arjunsankar24@gmail.com>
2020-04-23 16:19:38 +0530

    service page added

--- src/Datas/Service.js
+++ src/Datas/Service.js
@@ -6,6 +6,30 @@ import prlogo from "../Images/service_img/Group 8.png";
 import sblogo from "../Images/service_img/Group 9.png";
 import seconShutterLogo from "../Images/service_img/shutterstock_331398086 copy.png";
 
+//third shutter logos
+import thirdShutterLogo from "../Images/service_img/shutterstock_331398086 copy 2.png";
+
+import serviceDeskLogo from  "../Images/service_img/Group 10.png";
+
+ import ENSlogo from  "../Images/service_img/Group 11.png";
+import CloudServicesLogo from  "../Images/service_img/Group 12.png";
+import SISlogo from  "../Images/service_img/Group 13.png";
+import IESLogo from  "../Images/service_img/Group 14.png";
+
+//fourth shutter logos
+import infraLogo from  "../Images/service_img/shutterstock_331398086 copy 3.png";
+
+//fifth shutter logos
+import EducationLogo from  "../Images/service_img/shutterstock_331398086 copy 4.png";
+
+//final
+import helpblockChainlogo from  "../Images/service_img/Group 15.png";
+import underStandlogo from  "../Images/service_img/Group 16.png";
+import LeadershipPieceLogo from  "../Images/service_img/Group 17.png";
+import explainBlockChainlogo from  "../Images/service_img/Group 18.png";
+import aspectsBlockchainLogo from  "../Images/service_img/Group 19.png";
+
+
 
 
 
@@ -35,6 +59,50 @@ BusinessConsulting:"Business Consulting",
 secondContent1:"Our leadership team possesses a unique blend of real-world experience. We will ensure that BOTH the people and technology changes needed for business transformation are identified and shared.Using our expertise, we will help define both “the tactical and strategic” using synergistic change management programs to address both.We will look across the business, technology, and customer ecosystems at all the underlying components (people, digital technologies, business processes, and tools) to understand and recommend appropriate strategies and tactics (leveraging digital technologies) to succeed.",
 secondContent2:"Using our expertise, we will help define both “the tactical and strategic” using synergistic change management programs to address both.",
 secondContent3:"We will look across the business, technology, and customer ecosystems at all the underlying components (people, digital technologies, business processes, and tools) to understand and recommend appropriate strategies and tactics (leveraging digital technologies) to succeed.",
-seconShutterLogo:seconShutterLogo
+seconShutterLogo:seconShutterLogo,
+
+//shutter third
+thirdShutterLogo:thirdShutterLogo,
+TechnologyConsulting:"Technology Consulting",
+tecText:"In the new digital economy, we assist organizations across industry verticals to adapt. We help them look at potentially disruptive next-gen technologies such as Robotic Process Automation (RPA), Blockchain, the Internet of Things (IoT), and Machine Learning to consider. Our services are designed to ensure security and efficiency of IT environments and operations, which in turn reduces the risk of loss, improves overall business performance as well as address any required compliance with data regulations in the respective industry.",
+
+//shutter four
+infraLogo:infraLogo,
+InfrastructureManagement:"Infrastructure Management",
+imText:"The ongoing digital disruptions across industries has given rise to a new, hybrid infrastructure approach to environments that requires significant expertise to sort out. Without a clear path, environments can become overly complex and not business focused. We feel that enterprises’ need a single version of truth across their technology and application landscape. Our next generation infrastructure management services including RapidQube's Cloud-First strategy.",
+headingIm:"Infrastructure Management service portfolio includes:",
+serviceDeskLogo:serviceDeskLogo,
+ServicDesk:"Service desk",
+ENSlogo:ENSlogo,
+EnterpriseNetworkServices:"Enterprise Network Services",
+CloudServicesLogo:CloudServicesLogo,
+CloudServices:"Cloud Services",
+SISlogo:SISlogo,
+SystemIntegrationServices:"System Integration Services",
+IESLogo:IESLogo,
+InfrastructureEngineeringServices:'Infrastructure Engineering Services',
+
+
+//fifth shutter
+EducationLogo:EducationLogo,
+Education:'Education',
+innerContentEdu1:'We feel that as technologies change, the approach towards education also needs to change.',
+innerContentEdu2:"RapidQube has delivered Fortune 500 (India/ Webinar), IIT Bombay (India), and Connecticut (UCONN, CCSU) University level blockchain educational sessions (not for credit) to date. Our goal is to spread the word about the technology, its capabilities, and ensure that colleges, universities, and global businesses are benefitting from it.",
+innerContentEdu3:"We hosted over 200 hours of industry specific Use Case/ solution discussions. Each of these sessions required research, development, and presentation specific fine-tuning to ensure that the message was appropriate.",
+
+
+// final
+session:'Our intent is that these sessions do the following',
+helpblockChainlogo:helpblockChainlogo,
+helpblockChainText:"Help You Define Blockchain Technology In Your Own Words",
+underStandlogo:underStandlogo,
+understandText:'Understand How Blockchain Could Potentially Impact Your Business And Industry',
+LeadershipPieceLogo:LeadershipPieceLogo,
+LeadershipPieceText:'Write A Thought Leadership Piece Regarding Use Cases And Industry Potential Of Blockchain Technology',
+explainBlockChainlogo:explainBlockChainlogo,
+explainBlockChainText:'Explain Blockchain Technology To Your Clients, Friends, Business Colleagues',
+aspectsBlockchainLogo:aspectsBlockchainLogo,
+aspectsBlockchainText:"Identify Which Aspects Of Blockchain Technology Seem Most Important To You And Why",
+
 
 }
--- src/Files/Services.js
+++ src/Files/Services.js
@@ -2,12 +2,12 @@ import React from "react"
 import serviceData from "../Datas/Service"
 function Services() {
     return (
-        <div >
+        <div>
             <div className="text-center bg-blue-100">
                 <h4>{serviceData.services}</h4>
                 <p>{serviceData.servheadercontent}</p>
             </div>
-            <div className='flex'>
+            <div className='md:flex sm:flex'>
                 <div className="w-full w-1/2 p-3">
                     <img src={serviceData.shutterLogo} alt="shutter" />
                 </div>
@@ -57,27 +57,148 @@ function Services() {
 
 
                 </div>
-           
-           
+
+
             </div>
+
             {/* //second schutter */}
-<div className="flex bg-blue-100">
-    <div className=" w-full w-1/2 ">
-<div className="py-3">
-    <p className="font-bold  px-3 text-2xl py-3">{serviceData.BusinessConsulting}</p>
-    <div className="">
-    <p className="text-sm px-3 py-2">{serviceData.secondContent1}</p>
-    <p className="text-sm px-3 py-2">{serviceData.secondContent2}</p> 
-    <p className="text-sm px-3 py-2">{serviceData.secondContent3}</p>
-    </div>
+            <div className="md:flex sm:flex bg-blue-100">
+                <div className=" w-full w-1/2 ">
+                    <div className="py-3">
+                        <p className="font-bold  px-3 text-2xl py-3">{serviceData.BusinessConsulting}</p>
+                        <div className="">
+                            <p className="text-sm px-3 py-2">{serviceData.secondContent1}</p>
+                            <p className="text-sm px-3 py-2">{serviceData.secondContent2}</p>
+                            <p className="text-sm px-3 py-2">{serviceData.secondContent3}</p>
+                        </div>
 
-</div>
-    </div>
-    <div className=" w-full w-1/2  px-3 py-3">
-<img src={serviceData.seconShutterLogo} />
-    </div>
+                    </div>
+                </div>
+                <div className=" w-full w-1/2  px-3 py-3">
+                    <img src={serviceData.seconShutterLogo} alt="" />
+                </div>
 
-</div>
+            </div>
+
+
+            {/* //third shutter */}
+            <div className='md:flex sm:flex'>
+                <div className="w-full w-1/2 p-3">
+                    <img src={serviceData.thirdShutterLogo} alt="shutter" />
+                </div>
+                <div className="w-full w-1/2">
+                    <div>
+                        <p className="font-bold  px-3 text-xl py-3">{serviceData.TechnologyConsulting}</p>
+                        <p className="text-sm p-3">{serviceData.tecText}</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* //fourth shutter */}
+            <div className="bg-blue-100">
+                <div className="md:flex sm:flex ">
+                    <div className=" w-1/2 ">
+                        <div className="p">
+                            <p className="font-bold  px-3 text-2xl py-3">{serviceData.InfrastructureManagement}</p>
+                            <div className="py-2">
+                                <p className="text-sm px-3 py-2">{serviceData.imText}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="  w-1/2  px-3 py-3">
+                        <img className="" src={serviceData.infraLogo} alt="" />
+                    </div>
+
+                </div>
+                <div className="px-3">
+                    <h3>{serviceData.headingIm}</h3>
+                    <div className=" py-4">
+                        <div className="md:flex sm:flex">
+                            <div className=" flex">
+                                <img className="h-5 w-5" src={serviceData.serviceDeskLogo} alt="prlogo" />
+                                <label className="px-2 text-xs">{serviceData.ServicDesk}</label>
+                            </div>
+                            <div className="flex">
+                                <img className="h-5 w-5 " src={serviceData.ENSlogo} alt="prlogo" />
+                                <label className="px-2 text-xs">{serviceData.EnterpriseNetworkServices}</label>
+                            </div>
+                            <div className="flex">
+                                <img className="h-5 w-5" src={serviceData.CloudServicesLogo} alt="prlogo" />
+                                <label className="px-2 text-xs">{serviceData.CloudServices}</label>
+                            </div>
+                        </div>
+                        <div className="sm:flex py-3">
+                            <div className="flex">
+                                <img className="h-5 w-5" src={serviceData.SISlogo} alt="prlogo" />
+                                <label className="px-2 text-xs">{serviceData.SystemIntegrationServices}</label>
+                            </div>
+                            <div className="flex">
+                                <img className="h-5 w-5" src={serviceData.IESLogo} alt="prlogo" />
+                                <label className="px-2 text-xs">{serviceData.InfrastructureEngineeringServices}</label>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div>
+                        <div >
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            {/* Fifth shutter */}
+            <div className='md:flex sm:flex'>
+                <div className="w-full w-1/2 p-3">
+                    <img src={serviceData.EducationLogo} alt="shutter" />
+                </div>
+                <div className="w-full w-1/2">
+                    <div>
+                        <p className="font-bold  px-3 text-xl py-3">{serviceData.Education}</p>
+                        <p className="text-sm p-3">{serviceData.innerContentEdu1}</p>
+                        <p className="text-sm p-3">{serviceData.innerContentEdu2}</p>
+                        <p className="text-sm p-3">{serviceData.innerContentEdu3}</p>
+
+                    </div>
+
+                </div>
+            </div>
+            <div className="w-full text-center py-3">
+                <h3 className="font-bold">{serviceData.session}</h3>
+                <div className="flex w-full justify-center">
+
+                    <div className="w-1/4 text-right">
+                        <img src={serviceData.helpblockChainlogo} alt="" />
+
+                    </div>
+                    <div className="w-3/4">
+                        <p>{serviceData.helpblockChainText}</p>
+
+                    </div>
+
+                </div>
+                <div className="flex w-full justify-center">
+                    <div className="w-1/4 text-right">
+                        <img src={serviceData.underStandlogo} alt="" />
+                    </div>
+                    <div className="w-3/4">
+                        <p>{serviceData.understandText}</p>
+                    </div>
+                </div>
+                <div className="flex w-full justify-center">
+                    <img src={serviceData.LeadershipPieceLogo} alt="" />
+                    <p>{serviceData.LeadershipPieceText}</p>
+                </div>
+                <div className="flex w-full justify-center">
+                    <img src={serviceData.explainBlockChainlogo} alt="" />
+                    <p>{serviceData.explainBlockChainText}</p>
+                </div>
+                <div className="flex w-full justify-center">
+                    <img src={serviceData.aspectsBlockchainLogo} alt="" />
+                    <p>{serviceData.aspectsBlockchainText}</p>
+                </div>
+
+            </div>
 
         </div>
     )
